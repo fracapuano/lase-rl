2b99334a1bfef8de1c92233b0e2caa26ebb26351
Francesco Capuano <francesco_capuano@aol.com>
2025-02-22 17:31:41 +0100

    add: eval script for trained models

---
+++ eval.py
@@ -0,0 +1,37 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+import laserenv
+
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+
+def main():
+    render = True
+    def make_env():
+        env = gym.make("LaserEnv", render_mode="human" if render else "rgb_array")
+        return env
+    
+    env = VecFrameStack(DummyVecEnv([make_env]), n_stack=5)
+    model = PPO.load("runs/misty-feather-58/model.zip")
+    
+    # Evaluation loop
+    average_reward = 0
+    for _ in range(10):
+        obs = env.reset()
+        episode_reward = 0
+        done = False
+        truncated = False
+        while not (done or truncated):
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, done, info = env.step(action)
+            episode_reward += reward
+
+            if render:
+                env.render()
+    
+        average_reward += episode_reward
+
+    print(f"Average Reward: {average_reward / 10}")
+    env.close()
+
+if __name__ == "__main__": 
+    main()
