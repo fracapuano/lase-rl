d27492ce29b62a97142b47a2ed42161e823c6571
Francesco Capuano <francesco_capuano@aol.com>
2025-02-25 12:56:03 +0100

    add: randomized vec env for testing

--- eval.py
+++ eval.py
@@ -2,22 +2,46 @@ import gymnasium as gym
 from stable_baselines3 import PPO, SAC
 import laserenv
 
+import numpy as np
+
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from utils import make_wrapped_environment
+from laserenv.RandomVecEnv import RandomDummyVecEnv
+from argparse import Namespace
+from laserenv.env_utils import EnvParametrization
 
 def main():
     render = True
+    dr = False  # whether to randomize or not at eval time
+    env_name = "RandomLaserEnv"
+
+    env = make_vec_env(
+        env_name,
+        n_envs=1,
+        seed=0,
+        vec_env_cls=RandomDummyVecEnv,
+        wrapper_class=make_wrapped_environment,
+        wrapper_kwargs={'args': Namespace(stack_history=5)},
+        env_kwargs={'render_mode': 'human' if render else 'rgb_array'}
+    )
     
-    def make_env():
-        env = gym.make("LaserEnv", render_mode="human" if render else "rgb_array")
-        return env
-    
-    env = VecFrameStack(DummyVecEnv([make_env]), n_stack=5)
-    model = SAC.load("runs/vivid-sponge-110/model.zip")
+    gt_task = gym.make(env_name).unwrapped.get_task()
+    bounds_low, bounds_high = EnvParametrization().get_bounds(parameters=["B", "GDD"])
+
+    env.set_dr_distribution(
+        dr_type='uniform', 
+        distr=[x for i in range(len(gt_task)) for x in (bounds_low[i], bounds_high[i])]
+    )
+    env.env_method("set_dr_training", dr)
+
+    model = SAC.load("best_model.zip")
     
     # Evaluation loop
     average_reward = 0
     for _ in range(10):
         obs = env.reset()
+        
         episode_reward = 0
         done = False
         truncated = False
